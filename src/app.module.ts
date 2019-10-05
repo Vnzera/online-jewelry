698c83c481f57363e57cb3b7c316691e0f0093af
Luis Diaz <luis@linuxmail.org>
2019-10-05 14:07:33 -0700

    Added back-end files and set up Concurrently to run both dev servers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
@@ -7,7 +6,6 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule,
     AuthModule,
   ]
 })
