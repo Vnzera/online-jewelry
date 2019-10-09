d48e1d8b2963c46ecf1a5f459e649820fb7c5433
Luis Diaz <luis@linuxmail.org>
2019-10-08 22:44:55 -0700

    Added login component and minor edits

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,18 +1,11 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import Link from 'react-router-dom';
 
-// may change name of this component to 'SignUp'
-// edit server logic so that name, cart and saveForLater fields are added and username field is replaced by email
-// grab current items in cart and saveForLater if there are any and use those to set values in case the user started shopping before sign up
-
-// connect each input field to the state through value={input}
-// add a reusable onChange that only targets the specific state value for the input field 
-// ie [e.target.name] which grabs the name attribute of the input field ie email or password1 etc
-
-// check to see if passwords match onSubmit
-// if match then proceed to construct the registration post request to the server
+// edit server logic so that name, email, cart and saveForLater fields are added
+// grab current items in cart and saveForLater from localStorage if there are any and use those to set values in case the user started shopping before sign up
 
 const Register = () => {
+    // reach into localStorage and grab cart/saveForLater items
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -22,84 +15,70 @@ const Register = () => {
 
     const { name, email, password, password2 } = formData;
 
+    // connect each input field to the state through value={input}
+    // add a reusable onChange that only targets the specific state value for the input field 
+    // ie [e.target.name] which grabs the name attribute of the input field ie email or password1 etc
+
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
     const onSubmit = async e => {
+        // check to see if passwords match onSubmit
+        // if match then proceed to construct the registration post request to the server
         e.preventDefault();
         if (password !== password2) {
             console.log('Passwords do not match!');
         } else {
-            const newUser = {
-                name,
-                email,
-                password
-            };
-
-            try {
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                };
-
-                const body = JSON.stringify(newUser);
-
-                const res = await axios.post('http://localhost:5000/auth/signup', body, config);
-                console.log(res.data);
-            } catch (err) {
-                console.log(err.response.data);
-            }
+            console.log('Success!')
         }
+
+        return (
+            <>
+                <h1 className="large text-primary">Sign Up</h1>
+                <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+
+                <form className="form" onSubmit={e => onSubmit(e)}>
+
+                    <div className="form-group">
+                        <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+                    </div>
+
+                    <div className="form-group">
+                        <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
+                        <small className="form-text"
+                        >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+                        >
+                    </div>
+
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            value={password} onChange={e => onChange(e)}
+                            minLength="6"
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            placeholder="Confirm Password"
+                            name="password2"
+                            value={password2} onChange={e => onChange(e)}
+                            minLength="6"
+                        />
+                    </div>
+
+                    <input type="submit" className="btn btn-primary" value="Register" />
+
+                </form>
+
+                <p className="my-1">
+                    Already have an account? <Link to='/login'>Log In</Link>
+                </p>
+            </>
+        )
     }
 
-    return (
-        <>
-            <h1 className="large text-primary">Sign Up</h1>
-            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-
-            <form className="form" onSubmit={e => onSubmit(e)}>
-
-                <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
-                </div>
-
-                <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
-                    <small className="form-text"
-                    >This site uses Gravatar so if you want a profile image, use a
-            Gravatar email</small
-                    >
-                </div>
-
-                <div className="form-group">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        value={password} onChange={e => onChange(e)}
-                        minLength="6"
-                    />
-                </div>
-
-                <div className="form-group">
-                    <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        name="password2"
-                        value={password2} onChange={e => onChange(e)}
-                        minLength="6"
-                    />
-                </div>
-
-                <input type="submit" className="btn btn-primary" value="Register" />
-
-            </form>
-
-            <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
-            </p>
-        </>
-    )
-}
-
-export default Register;
+    export default Register;
