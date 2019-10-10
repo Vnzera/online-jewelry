2a48bc74341bc0b703e42b3d492f79ff1138c513
Luis Diaz <luis@linuxmail.org>
2019-10-10 02:01:52 -0700

    Set up actions and reducers for alerts

---
+++ client/src/actions/alert.js
@@ -0,0 +1,14 @@
+import uuid from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+// this action can be called from the component
+// this will trigger the Reducer logic aka switch case for this action.hljs-type
+// the store will be updated and changes sent to components that are subscribed
+
+export const setAlert = (msg, alertType) => dispatch => {
+    const id = uuid.v4();
+    dispatch({
+        type: SET_ALERT,
+        payload: { msg, alertType, id }
+    });
+};
---
+++ client/src/actions/types.js
@@ -0,0 +1,4 @@
+// having a folder for action types makes it easier to find them and edit them all in one place
+
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
