b1aa09dc0d081d644b8a0137c9a9ddbf870edc33
Luis Diaz <luis@linuxmail.org>
2019-10-12 23:25:50 -0700

    Auth action for account registration

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,7 @@
 // having a folder for action types makes it easier to find them and edit them all in one place
 
+// the action logic sort of describes an event and passes on relevant data to the Reducers such as ids
+
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
